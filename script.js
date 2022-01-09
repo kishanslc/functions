e84163727d244ae66a9b0b268a57e6415b6124b4
kishanslc <kishany.slc@gmail.com>
2022-01-09 13:15:58 -0500

    commir

---
+++ script.js
@@ -0,0 +1,246 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum,numPassengers=1,price =199){
+    //ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123',2,800);
+
+const flight = 'Lh234';
+const jonas = {
+    name:'Jonas Schmedtmann',
+    passport: 24739479284
+}
+
+const checkIn = function(flightNum,passenger){
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 24739479284){
+        alert('Checked in')
+    }
+    else{
+        alert('Wrong passport')
+    }
+}
+
+//checkIn(flight,jonas);
+//console.log(flight);
+//console.log(jonas);
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random()*1000000000000);
+
+}
+newPassport(jonas);
+checkIn(flight,jonas);
+
+const oneWord = function(str){
+    return str.replace(/ /g,' ').toLowerCase();
+};
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return[first.toUpperCase(), ...others].join(' ');
+};
+
+
+//Higher-order function 
+
+const transformer = function(str,fn){
+    console.log('Original string: ${str}');
+    console.log('Transformed string: ${fn(str)}');
+
+    console.log('Transformed by: ${fn.name}');
+
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!',oneWord);
+
+const high5 = function(){
+    console.log(' 👏');
+}
+document.body.addEventListener('click',high5);
+['Jonas','Martha','Adam'].forEach(high5);
+
+//function returning new functions
+
+const greet = function(greeting){
+    return function(name){
+        console.log('${greeting} ${name}');
+    };
+};
+
+const greeterHey = greet('Hey ${name}');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+//Challenge
+const greetArr = greeting => name => console.log('${greeting} ${name}');
+
+greetArr('Hi')('Jonas');
+
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    //book:function{}
+    book(flightNum,name){
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`);
+
+    },
+};
+
+lufthansa.book(239,'Kishan Yarlagadda');
+lufthansa.book(635,'John Smith');
+
+const eurowings = {
+    name: 'Eurowings',
+    iataCode: 'EW',
+    bookings:[],
+};
+
+const book = lufthansa.book;
+
+//Does NOT work
+//book(23,'Sarah Williams');
+
+book.call(eurowings,23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa,239,'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings:[],
+};
+book.call(swiss,583,'Mary Cooper');
+
+//Apply method 
+const flightData =[583,'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings,23);
+
+//With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function(){
+    console.log(this);
+
+    this.planes++;
+    console.log(this.planes);
+}
+
+
+
+//Partial application
+const addTax =(rate, value) => value + value*rate;
+console.log(addTax(0.1,200));
+
+const addVAT = addTax.bind(null,0.23);
+//addVAT = value => value+value*0.23;
+
+console.log(addVAT(100));
+
+const addTaxRate = function(rate){
+    return function(value){
+        return value+value*rate;
+    };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+const runOnce = function(){
+    console.log('This will never run again');
+};
+runOnce();
+
+//IIFE 
+(function(){
+    console.log('This will never run again');
+})();
+
+(() => console.log('This will also never run again'))();
+
+
+//closures 
+
+const secureBooking = function(){
+    let passengerCount = 0;
+
+    return function(){
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    };
+};
+
+const booker = secureBooking();
+ 
+booker();
+booker();
+booker();
+
+
+//Closure Example 1
+let f;
+const g = function(){
+    const a = 23;
+    f = function(){
+        console.log(a*23);
+    };
+};
+
+const h = function(){
+    const b = 777;
+    f = function(){
+        console.log(b*2);
+    };
+};
+
+g();
+f();
+console.dir(f);
+
+//Re-assigning f functions 
+h();
+f();
+console.dir(f);
+
+//closure example 2
+
+const boardPassengers = function(n,wait){
+    const perGroup = n / 3;
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    },wait*1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
